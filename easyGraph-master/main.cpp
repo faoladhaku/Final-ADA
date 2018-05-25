015a45a8545cffb6fe5eade4749c9e3aa41b5dfb
Unknown <angelsoulsword@gmail.com>
2018-05-25 09:58:01 -0500

    alex necesito la funcion de puntos mas cercanos
    :V:V:V:V:V:V:V:V:v

--- easyGraph-master/main.cpp
+++ easyGraph-master/main.cpp
@@ -1,12 +1,62 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include "interprete.h"
+#include <iostream>
+#include <ofstream>
+#include <vector>
+
+class quadtree()
+{
+
+}
+class coordenadas()
+{
+    vector<float> coordenada;
+};
+
+class puntos{
+public:
+    int eti;
+    vector<coordenadas> puntos;
+    int etianterior;
+    int age;
+    puntos();
+};
+
+puntos::puntos()
+{
+    this->age=0;
+    this->eti=0;
+    this->etianterior=0;
+}
+
+class gng{
+public:
+    gng();
+    void acercamiento(quadtree estructura);
+    void puntos();
+}
+
+vector<puntos> gng::puntos(puntos tiempos,vector<puntos> myvector)
+{
+    myvector.pushback(tiempos);
+}
+void gng::acercamiento(quadtree estructura)
+{
+    vector<puntos> newpuntos;
+    puntos mispuntos = new puntos();
+    for(;;)
+    {
+         puntos(newpuntos,mispuntos);
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    gng final;
+    vector<puntos> puntos = final.acercamiento();   
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-
     return a.exec();
 }
