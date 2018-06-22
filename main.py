bcbf50f6ea84098aae407dfea9fb97cbd6e85dd7
alrus2797 <alrus2797@gmail.com>
2018-06-22 03:58:20 -0500

    GNG v1.0

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from gng import gng
 import pygame as pg
 
-'''nodo1 = Nodo(1,[1,2],2)
-nodo2 = Nodo(2,[1,2],4)
 
-arista = Arista(nodo1,nodo2,2,2)
-
-grafo = Grafo()
-grafo.addNodo(nodo1)
-
-grafo.addNodo(nodo2)
-
-grafo.addArista(arista)
-
-grafo.findNodo(nodo1)'''
+#Ready to go: Linux and Windows
 
 def text_to_screen(screen, text, pos, size = 25):
     try:
